dd5901ba014c77ca88f8a3bf10b9d4b2a1453c0a
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-28 08:58:27 -0600

    contact form needs emailjs integration

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,14 @@
-import { useState, useRef } from "react"
-import emailjs from "@emailjs/browser"
-import dotenv from "dotenv"
-dotenv.config({ path: "../../.env" })
+import { useState } from "react"
 
 export default function Contact() {
 
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
-
-  const form = useRef()
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
 
   function handleFieldChange(e) {
     const { name, value } = e.target
-
     switch (name) {
       case "firstName":
         setFirstName(value)
@@ -20,73 +16,69 @@ export default function Contact() {
       case "lastName":
         setLastName(value)
         break
+      case "email":
+        setEmail(value)
+        break
+      case "message":
+        setMessage(value)
+        break
     }
   }
 
-  async function handleFormSubmit(e) {
-    e.preventDefault();
-
-    try {
-      const data = emailjs.sendForm(
-        process.env.EJS_SERVICE,
-        process.env.EJS_TEMPLATE,
-        form.current,
-        process.env.EJS_PUBLIC_KEY
-      )
-      console.info(data);
-    } catch (err) {
-      console.error(err);
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    const formResponse = {
+      firstName,
+      lastName,
+      email,
+      message,
     }
   }
 
   return (
     <form
-      className="bg-violet-900 p-8 rounded-2xl flex flex-col gap-2 min-h-[36rem]"
+      className="bg-violet-900 p-8 rounded-2xl flex flex-col gap-2"
       onSubmit={handleFormSubmit}
-      ref={form}
     >
-      <h3
-        className="text-2xl font-bold"
-      >
-        Hi{(firstName || lastName) && `, ${firstName} ${lastName}`}!
+      <h3 className="text-3xl font-bold">
+        Hello{(firstName || lastName) && `, ${firstName}${(firstName && lastName) && " "}${lastName}`}!
       </h3>
-      <p
-        className="text-lg pb-2"
-      >
+      <p className="text-xl font-semibod pb-2">
         I'm looking forward to hearing from you.
       </p>
       <div className="flex flex-row gap-2">
         <input
-          className="bg-violet-600 placeholder-violet-400 p-2 text-lg rounded-md"
+          className="bg-violet-600 placeholder-violet-400 p-2 rounded-lg text-lg"
           name="firstName"
-          placeholder="First Name"
           type="text"
+          placeholder="First Name"
           onChange={handleFieldChange}
         />
         <input
-          className="bg-violet-600 placeholder-violet-400 p-2 text-lg rounded-md"
+          className="bg-violet-600 placeholder-violet-400 p-2 rounded-lg text-lg"
           name="lastName"
-          placeholder="Last Name"
           type="text"
+          placeholder="Last Name"
           onChange={handleFieldChange}
         />
       </div>
       <input
-        className="bg-violet-600 placeholder-violet-400 p-2 text-lg rounded-md"
+        className="bg-violet-600 placeholder-violet-400 p-2 rounded-lg text-lg"
         name="email"
-        placeholder="E-Mail Address"
         type="email"
+        placeholder="E-Mail Address"
+        onChange={handleFieldChange}
       />
       <textarea
-        className="bg-violet-600 placeholder-violet-400 p-2 text-lg rounded-md h-full resize-none"
-        name="message"
-        placeholder="Message"
+        className="bg-violet-600 placeholder-violet-400 p-2 rounded-lg text-lg"
+        placeholder="Perhaps a nice message?"
+        onChange={handleFieldChange}
       />
-      <button 
+      <button
+        className="bg-violet-400 font-semibold hover:bg-violet-600 p-2 rounded-lg text-lg min-h-fit"
         type="submit"
-        className="bg-violet-400 hover:bg-violet-600 p-2 text-lg rounded-md w-full"
       >
-        Submit
+        LGTM
       </button>
     </form>
   )
