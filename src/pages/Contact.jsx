19de43d9f60a8f9e8b64453989dd10f8bc70ea17
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-28 18:49:27 -0600

    feat: contact form working

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,37 +1,50 @@
-import { useState } from "react"
+import { useState, useRef } from "react";
+import { sendForm } from "@emailjs/browser";
 
 export default function Contact() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  const form = useRef();
 
   function handleFieldChange(e) {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     switch (name) {
       case "firstName":
-        setFirstName(value)
-        break
+        setFirstName(value);
+        break;
       case "lastName":
-        setLastName(value)
-        break
+        setLastName(value);
+        break;
       case "email":
-        setEmail(value)
-        break
+        setEmail(value);
+        break;
       case "message":
-        setMessage(value)
-        break
+        setMessage(value);
+        break;
     }
   }
 
-  function handleFormSubmit(e) {
-    e.preventDefault()
-    const formResponse = {
-      firstName,
-      lastName,
-      email,
-      message,
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const result = await sendForm(
+        "service_xf67cxh",
+        "template_zao32ll",
+        e.target,
+        "BLXWK9ZkdcnOLC5RB"
+      );
+      console.info(result);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setMessage("");
     }
   }
 
@@ -39,9 +52,13 @@ export default function Contact() {
     <form
       className="bg-violet-900 p-8 rounded-2xl flex flex-col gap-2"
       onSubmit={handleFormSubmit}
+      ref={form}
     >
       <h3 className="text-3xl font-bold">
-        Hello{(firstName || lastName) && `, ${firstName}${(firstName && lastName) && " "}${lastName}`}!
+        Hello
+        {(firstName || lastName) &&
+          `, ${firstName}${firstName && lastName && " "}${lastName}`}
+        !
       </h3>
       <p className="text-xl font-semibod pb-2">
         I'm looking forward to hearing from you.
@@ -53,6 +70,7 @@ export default function Contact() {
           type="text"
           placeholder="First Name"
           onChange={handleFieldChange}
+          value={firstName}
         />
         <input
           className="bg-violet-600 placeholder-violet-400 p-2 rounded-lg text-lg"
@@ -60,6 +78,7 @@ export default function Contact() {
           type="text"
           placeholder="Last Name"
           onChange={handleFieldChange}
+          value={lastName}
         />
       </div>
       <input
@@ -68,11 +87,13 @@ export default function Contact() {
         type="email"
         placeholder="E-Mail Address"
         onChange={handleFieldChange}
+        value={email}
       />
       <textarea
         className="bg-violet-600 placeholder-violet-400 p-2 rounded-lg text-lg"
         placeholder="Perhaps a nice message?"
         onChange={handleFieldChange}
+        value={message}
       />
       <button
         className="bg-violet-400 font-semibold hover:bg-violet-600 p-2 rounded-lg text-lg min-h-fit"
@@ -81,5 +102,5 @@ export default function Contact() {
         LGTM
       </button>
     </form>
-  )
+  );
 }
