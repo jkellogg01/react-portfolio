1fd1414d08cd7e80012a2f256f6cde99adb59921
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-29 20:57:35 -0600

    moved ResumeSection component into Resume page. I will* go back and figure out how to break that back out to its own file

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,11 @@
-import { ResumeSection } from "../components/ResumeSection";
+function ResumeSection({ title, children }) {
+  return (
+    <div className="flex flex-col gap-4 bg-violet-800 p-4 rounded-lg">
+      {title && <h4 className="text-2xl font-semibold">{title}</h4>}
+      {children}
+    </div>
+  );
+}
 
 export default function Resume() {
   const technicalSkills = {
